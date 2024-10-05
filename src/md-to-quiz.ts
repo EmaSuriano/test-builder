ce5a4e7b5a4f576afac93e5803236ebea0a5d2a6
Ema <emasuriano@MacBook-Pro-von-Ema.local>
2024-10-05 17:39:30 +0200

    format

--- src/md-to-quiz.ts
+++ src/md-to-quiz.ts
@@ -3,6 +3,28 @@ import { JSDOM } from "jsdom";
 import * as fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 
+async function markdownToDocument(doc: string): Promise<Document> {
+  // Convert Markdown to HTML
+  let html = await marked(doc);
+
+  // Regular expression to match headers and content
+  const sectionRegex = /(<h2>.*?<\/h2>)(.*?)(?=<h[1-6]>|$)/gs;
+
+  // Wrap matched sections in <section> tags
+  html = html.replace(
+    sectionRegex,
+    (_, header, content) => `<section>${header}${content.trim()}</section>`,
+  );
+
+  console.log(html);
+
+  const dom = new JSDOM(html);
+
+  return dom.window.document;
+}
+
+type NonEmptyArray<T> = [T, ...T[]]; // At least one element
+
 type Question = {
   title: string;
   description: string;
@@ -11,41 +33,51 @@ type Question = {
   meta?: string;
 };
 
+type CompleteQuestion = {
+  title: string;
+  description: string;
+  choices: NonEmptyArray<string>;
+  correctAnswer: string;
+  meta?: string;
+};
+
 const { DOC_PATH } = process.env;
 if (!DOC_PATH) {
-  throw Error("Please specify doc for quiz");
+  throw Error("Please specify path of the doc, by sending DOC_PATH as env");
 }
 
-const doc = fs.readFileSync(DOC_PATH, "utf8");
+const getNodeText = (elem): string => elem.textContent?.trim() || "";
 
-// Convert markdown to HTML using `marked`
-const html = await marked(doc);
-
-// Parse the HTML using JSDOM
-const dom = new JSDOM(html);
-const document = dom.window.document;
+const document = await markdownToDocument(fs.readFileSync(DOC_PATH, "utf8"));
 
 const title = document.querySelector("h1")?.textContent || "";
 
 // Create an array to hold all questions
-const questions: Question[] = [];
+const questions: CompleteQuestion[] = [];
 
 // Get all sections (each question type like Checkbox, Radio button, etc.)
-document.querySelectorAll("h2").forEach((section) => {
+document.querySelectorAll("section").forEach((section) => {
   const question: Question = {
-    title: section.textContent || "",
+    title: "",
     description: "",
     meta: "",
     choices: [],
     correctAnswer: [],
   };
 
-  let currElem = section.nextElementSibling!;
+  section.childNodes.forEach((currElem) => {
+    if (!(currElem instanceof HTMLElement)) {
+      return;
+    }
 
-  while (currElem && currElem.tagName !== "H2") {
+    console.log(currElem.tagName);
     switch (currElem.tagName) {
+      case "H2":
+        question.title = getNodeText(currElem);
+        break;
+
       case "BLOCKQUOTE":
-        question.description = currElem.textContent?.trim() || "";
+        question.description = getNodeText(currElem);
         break;
 
       case "UL": {
@@ -53,10 +85,11 @@ document.querySelectorAll("h2").forEach((section) => {
         const options = liElements.map((li) => {
           const input = li.querySelector("input");
           const checked = input?.getAttribute("checked") !== null; // Check if the input is marked as checked
-          const label = li.textContent?.trim() || ""; // Get the label text
+          const label = getNodeText(li);
 
           return { label, checked };
         });
+
         question.choices = options.map((opt) => opt.label);
         question.correctAnswer = options
           .filter((opt) => opt.checked)
@@ -66,17 +99,31 @@ document.querySelectorAll("h2").forEach((section) => {
 
       // if not recognized, then it's extra attributes
       default:
-        question.meta += currElem.outerHTML;
+        if (currElem.outerHTML) {
+          question.meta += currElem.outerHTML;
+        }
         break;
     }
+  });
 
-    currElem = currElem.nextElementSibling!;
-  }
-
-  questions.push(question);
+  questions.push(validateQuestion(question));
 });
 
-const enhanceQuestion = (q: Question) => {
+const validateQuestion = (q: Question): CompleteQuestion => {
+  if (q.choices.length < 1) {
+    throw Error(`The following question "${q.title}" does not have choices`);
+  }
+
+  if (!q.correctAnswer) {
+    throw Error(
+      `The following question "${q.title}" does not have a correct answer`,
+    );
+  }
+
+  return q;
+};
+
+const enhanceQuestion = (q: CompleteQuestion) => {
   const { meta, ...rest } = q;
   const name = uuidv4();
 
